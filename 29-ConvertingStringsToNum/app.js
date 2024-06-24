69ffd998809908fbe6cafd6ee2a1a60f203ca848
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-06-24 12:37:51 +0500

    Add files via upload

---
+++ 29-ConvertingStringsToNum/app.js
@@ -0,0 +1,16 @@
+var integerstring = "28";
+var num = Number
+(integerstring);
+
+console.log(num);
+
+ var floatingNumString = "24.9876";
+var num = Number
+(floatingNumString);
+
+console.log(num);
+
+var num = 123456;
+
+console.log(num.toString());
+
